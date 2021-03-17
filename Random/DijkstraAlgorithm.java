4bdbcd86d093e96fbc520182d3c0d19499468f25
Aman Kulshreshtha <aman21102000@gmail.com>
2021-03-17 22:04:08 +0530

    Coding practice done

--- Random/DijkstraAlgorithm.java
+++ Random/DijkstraAlgorithm.java
@@ -1,10 +1,9 @@
 import java.util.*;
 import java.io.*;
 import java.lang.*;
-import java.math.BigInteger;
 
 
-public class CeilAndReceipt {
+public class DijkstraAlgorithm {
 
     static class FastReader {
         BufferedReader br;
@@ -49,31 +48,49 @@ public class CeilAndReceipt {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean[] sieve = new boolean[100001];
-        sieve[2] = true;
-        for (int i = 0; i < 100001; i++) {
-            sieve[i] = true;
-        }
-
-        for (int i = 2; i * i <= 100000; i++) {
-            if (sieve[i]) {
-                for (int j = i * i; j <= 100000; j += i) {
-                    sieve[j] = false;
-                }
-            }
-        }
-
+        FastReader sc = new FastReader();
         int t = sc.nextInt();
         while (t-- > 0) {
-            int x = sc.nextInt();
-            if (sieve[x]) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
+            int n = sc.nextInt();
+            var arr = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) {
+                arr.add(sc.nextInt());
             }
+
+            Collections.sort(arr);
+            var temp = new ArrayList<Integer>();
+            var ans = new ArrayList<Integer>();
+            var set = new HashSet<Integer>();
+            boolean flag = true;
+            while (flag) {
+                for (int x : arr) {
+                    if (set.contains(x)) {
+                        temp.add(x);
+                    } else {
+                        set.add(x);
+                        ans.add(x);
+                    }
+                }
+
+                if (temp.isEmpty()) {
+                    flag = false;
+                } else {
+                    arr.clear();
+                    arr.addAll(temp);
+                    temp.clear();
+                    set.clear();
+                }
+            }
+
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+
         }
-        sc.close();
+       
         System.exit(0);
+
     }
+
 }
